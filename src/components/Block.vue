b9bfb47eb2f7bf9453d9964967b0c4ee77214835
602822 <vefjeld@gmail.com>
2023-07-31 21:55:14 +0200

    finished

---
+++ src/components/Block.vue
@@ -0,0 +1,69 @@
+<template>
+    <div v-if="showBlock" @click="stopTimer" class="block">
+        click me
+    </div>
+</template>
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        }
+    },
+
+    mounted() {
+        console.log('mounted');
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+            console.log(this.delay);
+        }, this.delay)
+
+
+
+
+    }
+    ,
+    updated() {
+        console.log('updated');
+    },
+
+    unmounted() {
+        console.log('unmounted');
+
+    },
+
+    methods: {
+
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10)
+
+        },
+
+        stopTimer() {
+            clearInterval(this.timer);
+            console.log(this.reactionTime);
+            this.$emit('block-clicked', this.reactionTime);
+        },
+    }
+
+
+}
+
+</script>
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: lightgreen;
+    color: darkgreen;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template >
+    <div>
+        <p>Reaction time: <span class="score"> {{ score }}</span> ms</p>
+        <p>Rank: <span class="score"> {{ rank }}</span> </p>
+    </div>
+</template>
+<script>
+export default {
+    props: ['score', 'rank'],
+
+
+
+
+
+
+}
+
+</script>
+<style >
+.score {
+    color: blue;
+}
+
+p {
+    font-weight: bold;
+    font-family: monospace;
+    font-size: 1.5em;
+}
+</style>
